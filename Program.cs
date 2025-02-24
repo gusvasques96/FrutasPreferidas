92da4377dde523dfd8a8c4fa2b586b586322c970
gusvasques96 <gvasques.notes@gmail.com>
2025-02-24 11:06:28 -0300

    initial commit: adding project files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("--- Frutas Preferidas ---");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("1 - Maracujá");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("2 - Framboesa");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("3 - Limão");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.WriteLine("4 - Laranja");
+Console.ResetColor();
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("5 - Açaí");
+Console.ResetColor();
